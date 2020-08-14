89fb45b4191cf7c73e6b60b138ef6ecc907b80c0
Ahmed Essam <ahmed.essam@digitalwellnez.com>
2020-08-14 09:04:43 +0200

    Create RequestAppAdsPage.java

---
+++ src/test/java/pages/RequestAppAdsPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
+import org.openqa.selenium.support.PageFactory;
+
+public class RequestAppAdsPage extends BasePage {
+	
+	public AppiumDriver<AndroidElement> driver = null;
+
+	public RequestAppAdsPage(AppiumDriver<AndroidElement> driver2) 
+	{
+		this.driver = driver2;
+		PageFactory.initElements(new AppiumFieldDecorator(driver2), this);
+	}
+	
+	
+	
+	@AndroidFindBy(id="com.dgwell.caresquare.doctor:id/inapp_advert_image")
+	public AndroidElement AppAdsIcon;
+	
+	@AndroidFindBy(xpath="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/"
+			+ "android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/"
+			+ "android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/"
+			+ "android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/"
+			+ "androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.RadioButton")
+	public AndroidElement SelctedAdvert;
+	
+	@AndroidFindBy(id="com.dgwell.caresquare.doctor:id/btn_next")
+	public AndroidElement ContinueBtn;
+
+}
